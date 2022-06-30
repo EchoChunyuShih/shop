889924a08d12bee458f2d21e025096bfba81ba9a
Echo Shih <chunyushih@outlook.com>
2022-07-01 00:33:54 +0800

    [docs] RWD break points

--- src/global.css.js
+++ src/global.css.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
 #root {
-
     font-family: "IBM Plex Mono", monospace;
     /* color: #233342; */
     margin: 0;
@@ -19,10 +18,19 @@ export default createGlobalStyle`
     position: absolute;
     top: 0;
     left:0;
+    
+  }
+  input {
+      background-color: transparent;
+      border:none; 
+      border-bottom: 1px solid #ccc;
+      }
+   input:focus{
+    outline: none;
+   }
     a{
       text-decoration: none;
-    }
-  }`;
+    }`;
 
 export const theme = {
   colors: {
@@ -33,5 +41,13 @@ export const theme = {
   },
   fontFamily: {
     mono: "'IBM Plex Mono', monospace"
+  },
+  device: {
+    // mobileS: "(max-width: 320px)",
+    mobileM: "(max-width: 375px)",
+    mobileL: "(max-width: 425px)",
+    tablet: "(max-width: 768px)",
+    laptop: "(max-width:1024px)",
+    desktop: "(max-width: 2560px)"
   }
 };
